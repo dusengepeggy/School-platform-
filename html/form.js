543b7b513dd3220e8df8d592b0ea9ce56a1829bf
dusengepeggy <135857264+dusengepeggy@users.noreply.github.com>
2023-12-14 09:31:12 +0200

    Updation

--- html/form.js
+++
@@ -1,6 +0,0 @@
-
-    
-        function submitForm() {
-            // Add your form submission logic here
-            alert('Form submitted!'); // Replace with your actual submission code
-        }
