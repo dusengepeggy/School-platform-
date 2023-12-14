c198e924aa98eb7d910e6ab5c5b9b541b142a1c7
dusengepeggy <135857264+dusengepeggy@users.noreply.github.com>
2023-12-14 16:39:10 +0200

    responsiveness

---
+++ js/nav.js
@@ -0,0 +1,8 @@
+function hidebar(){
+    var bar=document.querySelector('.toshow');
+    bar.style.display="none";
+}
+function showbar() {
+    var bar=document.querySelector('.toshow');
+    bar.style.display='flex';
+}
